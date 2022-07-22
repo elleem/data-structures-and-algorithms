9d69c0b8b42995bbd7fc77b41379b2a5eb50a3a5
Lauren Main <lauren.main28@gmail.com>
2022-07-21 19:50:34 -0700

    solve for 5

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -70,7 +70,9 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  // uses filter to return an array of all the elements in the second array that are not included in the first array.
+  let newArr = arr.filter (number => !forbiddenValues.includes(number));
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
