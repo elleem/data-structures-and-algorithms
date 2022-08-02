638d12d2df02ce997114d79e3eae83e9b3967bb4
Lauren Main <lauren.main28@gmail.com>
2022-08-01 21:10:29 -0700

    solves 2

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -41,7 +41,9 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
+  // appends ' The end.' to a string, and returns the modified string
+  let end = str + ' The end.';
+  return end;
 };
 
 /* ------------------------------------------------------------------------------------------------
