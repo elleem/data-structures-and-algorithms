37a069f9ffc5adf23806bba4bf1e151d4854b738
Lauren Main <lauren.main28@gmail.com>
2022-08-13 14:11:36 -0700

    solves 3

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -28,7 +28,7 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 
 const getCourseKeys = (obj) => {
   // Solution code here...
-  return Object.keys(obj); 
+  return Object.keys(obj);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -41,6 +41,7 @@ Write a function named checkValues that takes in an object and a value and retur
 
 const checkValues = (obj, value) => {
   // Solution code here...
+  return Object.values(obj).includes(true);
 };
 
 /* ------------------------------------------------------------------------------------------------
