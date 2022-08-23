09cc97850d848477e374ae807b4d48ed71f9f017
Lauren Main <lauren.main28@gmail.com>
2022-08-23 04:29:41 -0700

    solves 4

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -48,6 +48,7 @@ If it does, return true. If not, return false.
 
 const hasNumber = (string) => {
   // Solution code here...
+  return /[a-zA-Z][0-9]/.test(string);
 };
 
 /* ------------------------------------------------------------------------------------------------
