6b896d9cc893df355f7f2fa0e6b753452fd37d38
Lauren Main <lauren.main28@gmail.com>
2022-08-26 04:22:23 -0700

    solves 2

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -69,7 +69,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 
 const validateEmail = (email) => {
   // Solution code here...
- return /^\w+?(\.\w+?)?@\w*(.com|.net|.org)$/.test(email);
+  return /^\w+?(\.\w+?)?@\w*(.com|.net|.org)$/.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
