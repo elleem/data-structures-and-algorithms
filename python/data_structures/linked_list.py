0ba48a3ab4456aad5965b515a95872aefb5f55e4
Lauren <lauren.main28@gmail.com>
2023-04-01 13:52:50 -0700

    passes first 4 tests, per class demo

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,24 @@
+class Node:
+    #create a node class with value and pointer to the next node
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class LinkedList:
     """
-    Put docstring here
+create a LinkedList class, include head, upon instantiation an empty LinkedList is created
     """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        if self.head is None:
+            self.head= Node(value)
+        else:
+            current=self.head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(value)
 
 
 class TargetError:
