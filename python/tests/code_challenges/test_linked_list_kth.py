042e2175fa817213eb0cc4dfd98748939874c75e
Lauren <lauren.main28@gmail.com>
2023-04-09 21:25:19 -0700

    adds additional tests and modifies kth_from_end after additional testing coverage

--- python/tests/code_challenges/test_linked_list_kth.py
+++ python/tests/code_challenges/test_linked_list_kth.py
@@ -62,3 +62,50 @@ def test_kth_from_end_size_one():
     actual = linked_list.kth_from_end(0)
     expected = "apples"
     assert actual == expected
+
+#the tests below authored by Lauren
+def test_kth_from_end_out_of_range():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    with pytest.raises(TargetError):
+        linked_list.kth_from_end(5)
+
+def test_kth_from_end_same_length():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    actual = linked_list.kth_from_end(len(values))
+    expected = "apples"
+    assert actual == expected
+
+def test_kth_from_end_negative_k():
+    linked_list = LinkedList()
+    values = ["apples", "bananas", "cucumbers"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    with pytest.raises(TargetError):
+        linked_list.kth_from_end(-2)
+
+def test_kth_from_end_size_one():
+    linked_list = LinkedList()
+    linked_list.insert("apple")
+    actual = linked_list.kth_from_end(0)
+    expected = "apple"
+    assert actual == expected
+
+
+def test_kth_from_end_middle():
+    linked_list = LinkedList()
+    values = ["apple", "banana", "cucumber", "durian", "orange"]
+    for value in reversed(values):
+        linked_list.insert(value)
+    actual = linked_list.kth_from_end(2)
+    expected = "cucumber"
+    assert actual == expected
+def test_kth_from_end_empty_list():
+    linked_list = LinkedList()
+    with pytest.raises(TargetError):
+        linked_list.kth_from_end(0)
