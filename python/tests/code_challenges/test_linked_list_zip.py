b9ad8b73eb4853a62b7071ddda31f6ca212f20d8
Lauren <lauren.main28@gmail.com>
2023-04-11 22:59:56 -0700

    adds additional tests beyond class provided tests

--- python/tests/code_challenges/test_linked_list_zip.py
+++ python/tests/code_challenges/test_linked_list_zip.py
@@ -86,3 +86,76 @@ def test_b_empty():
     for value in reversed([1, 2, 3]):
         expected.insert(value)
     assert str(actual) == str(expected)
+
+# tests after this note authored by Lauren
+
+def test_single_element_lists():
+    list_a = LinkedList()
+    for value in reversed([1]):
+        list_a.insert(value)
+    list_b = LinkedList()
+    actual = zip_lists(list_a, list_b)
+    expected = LinkedList()
+    for value in reversed([1]):
+        expected.insert(value)
+    assert str(actual) == str(expected)
+
+def test_both_empty():
+    list_a = LinkedList()
+    list_b = LinkedList()
+
+    actual = zip_lists(list_a, list_b)
+
+    expected = LinkedList()
+    assert str(actual) == str(expected)
+
+def test_repeated_lists():
+    list_a = LinkedList()
+    for value in reversed([1, 2, 3]):
+        list_a.insert(value)
+
+    list_b = LinkedList()
+    for value in reversed([1,2,3]):
+        list_b.insert(value)
+
+    actual = zip_lists(list_a, list_b)
+    expected = LinkedList()
+    for value in reversed([1, 1, 2, 2, 3, 3]):
+        expected.insert(value)
+
+    assert str(actual) == str(expected)
+
+def test_repeated_items_in_list():
+    list_a = LinkedList()
+    for value in reversed([1, 1, 1]):
+        list_a.insert(value)
+
+    list_b = LinkedList()
+    for value in reversed([1,2,3]):
+        list_b.insert(value)
+
+    actual = zip_lists(list_a, list_b)
+    expected = LinkedList()
+    for value in reversed([1, 1, 1, 2, 1, 3]):
+        expected.insert(value)
+
+    assert str(actual) == str(expected)
+
+
+def test_for_a_fun_message():
+    list_a = LinkedList()
+    for value in reversed(["Lauren", "enrolled", "Code"]):
+        list_a.insert(value)
+
+    list_b = LinkedList()
+    for value in reversed(["is", "at", "Fellows!"]):
+        list_b.insert(value)
+
+    actual = zip_lists(list_a, list_b)
+    expected = LinkedList()
+    for value in reversed(["Lauren", "is", "enrolled", "at", "Code", "Fellows!"]):
+        expected.insert(value)
+
+    assert str(actual) == str(expected)
+
+
