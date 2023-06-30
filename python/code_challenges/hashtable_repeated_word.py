419b3e2a775334280f3e8ad6c4cc6936b1885534
Lauren <lauren.main28@gmail.com>
2023-06-29 20:07:35 -0700

    reworks code without implementing hashtable class

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,14 +1,22 @@
-from data_structures.hashtable import Hashtable
+# from data_structures.hashtable import Hashtable
 import string
 
 
 def first_repeated_word(input_string):
-    words = [word.strip(string.punctuation).lower() for word in input_string.split()]
-    hashtable = Hashtable()
+    word_count = {}
+    words = input_string.lower().split()
 
 
     for word in words:
-        if hashtable.has(word):
+        #remove punctuation marks from the word
+        #thanks to TA assistance because translate was new to me, but I did end up trying split as well for readability
+        #I left split
+        #I learned that translate with create a translation table to remove punctuation characters
+        # word = word.translate(str.maketrans('','',string.punctuation))
+        word = word.strip(string.punctuation)
+
+        if word in word_count:
             return word
-        hashtable.set(word, None)
+        else:
+            word_count[word] = 1
     return None
--- python/tests/code_challenges/test_hashtable_repeated_word.py
+++ python/tests/code_challenges/test_hashtable_repeated_word.py
@@ -78,3 +78,36 @@ def test_punctuation_joins():
     actual = first_repeated_word(txt)
     expected = "banana"
     assert actual == expected
+
+
+#tests authored by Lauren below
+
+def test_single_word():
+    actual= first_repeated_word("hello")
+    expected = None
+    assert actual == expected
+
+def test_multiple_repeats():
+    actual = first_repeated_word("cat dog cat dog")
+    expected = "cat"
+    assert actual == expected
+
+def test_long_sentence():
+    actual = first_repeated_word("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...")
+    expected = "it"
+    assert actual == expected
+
+def test_my_smallest_meaningful_example():
+    actual = first_repeated_word("the cat and the dog")
+    expected = "the"
+    assert actual == expected
+
+def test_special_characters():
+    actual = first_repeated_word("!!!?!#@$$%$&^*(){}[]!!!??")
+    expected = None
+    assert actual == expected
+
+def test_immediate_repeat():
+    actual = first_repeated_word("June June July July")
+    expected = "june"
+    assert actual == expected
