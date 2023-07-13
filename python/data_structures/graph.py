0929a6c91fde27a2af9656a16c5519c2986d68a1
Lauren <lauren.main28@gmail.com>
2023-07-12 21:51:57 -0700

    adds updated graph implementation and passing tests

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,43 +1,36 @@
 class Graph:
-    """
-    Put docstring here
-    """
-
     def __init__(self):
         self.graph = {}
+
+    def size(self):
+        return len(self.graph)
 
     def add_node(self, value):
         node = Vertex(value)
         self.graph[node] = []
         return node
 
-    def add_edge(self,node1,node2,weight):
-        self.add_node(node1)
-        self.add_node(node2)
+    def add_edge(self, node1, node2, weight=0):
+        if node1 not in self.graph or node2 not in self.graph:
+            raise KeyError("Both 'node1' and 'node2' must be present in the graph.")
 
-        self.graph[node1].append([node2, weight])
-        self.graph[node2].append([node1, weight])
+        self.graph[node1].append(Edge(node2, weight))
 
-    def get_vertices(self):
-        return list(self.graph.keys())
-
-    def get_neighbors(self,vertex):
-        if vertex in self.graph:
-            neighbors = []
-            for neighbor, weight in self.graph[vertex]:
-                neighbor_vertex = Vertex(neighbor.value)
-                neighbor_vertex.weight = weight
-                neighbors.append(neighbor_vertex)
-            return neighbors
-        else:
-            return []
-
-    def size(self):
-        return len(self.graph)
+    def get_neighbors(self, node):
+        return self.graph[node]
 
     def get_nodes(self):
-        return self.graph
+        return self.graph.keys()
+
 
 class Vertex:
     def __init__(self, value):
         self.value = value
+
+
+class Edge:
+    def __init__(self, vertex, weight):
+        self.vertex = vertex
+        self.weight = weight
+
+
